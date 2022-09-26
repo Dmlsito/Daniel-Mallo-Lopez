a5a4628ea376c7eabf08b2267b6ce67b0fe1f423
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2022-09-26 14:29:42 +0200

    Ejercicio6 mitad hecho

---
+++ AD/20220912/untitled/src/main/java/ejercicio6/Alumno.java
@@ -0,0 +1,33 @@
+package ejercicio6;
+
+public class Alumno {
+    //Atributos de la clase alumno
+    String Nombre ;
+    int edadAlumno;
+    double notaMediaAlumno;
+
+
+
+
+    //Constructor
+    public Alumno(String NombreAlumno, int edad, double notaMedia) {
+
+        Nombre = NombreAlumno;
+        edadAlumno = edad;
+        notaMediaAlumno  = notaMedia;
+    }
+
+    //Metodo get
+    String getName(){
+
+        return Nombre;
+    }
+
+    int getEdad(){
+        return edadAlumno;
+    }
+
+    double getNotaMedia(){
+        return notaMediaAlumno;
+    }
+}
