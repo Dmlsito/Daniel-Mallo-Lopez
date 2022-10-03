c9ab70542c5a44017a003cb024c1e469488f875e
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2022-10-03 10:31:10 +0200

    Escritura XML

--- AD/20220912/Clase/Clase/Form1.Designer.cs
+++ AD/20220912/Clase/Clase/Form1.Designer.cs
@@ -32,11 +32,14 @@ namespace Clase
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,6 +55,9 @@ namespace Clase
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.LightCyan;
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
             this.panel2.Controls.Add(this.button3);
             this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button1);
@@ -59,6 +65,57 @@ namespace Clase
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(197, 580);
             this.panel2.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.SlateBlue;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(-1, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 43);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Ejercicio5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SlateBlue;
+            this.button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(3, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 38);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Ejercicio4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoEllipsis = true;
+            this.button3.BackColor = System.Drawing.Color.SlateBlue;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(0, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 40);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Ejercicio3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SlateBlue;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(-1, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 40);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Ejercicio2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -89,30 +146,18 @@ namespace Clase
             this.panel4.Size = new System.Drawing.Size(1352, 580);
             this.panel4.TabIndex = 3;
             // 
-            // button2
+            // button6
             // 
-            this.button2.BackColor = System.Drawing.Color.SlateBlue;
-            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(-1, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(198, 40);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Ejercicio2";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.AutoEllipsis = true;
-            this.button3.BackColor = System.Drawing.Color.SlateBlue;
-            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(0, 78);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(197, 40);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Ejercicio3";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button6.BackColor = System.Drawing.Color.SlateBlue;
+            this.button6.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(3, 227);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 40);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Ejercicio7";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Formulario
             // 
@@ -144,6 +189,9 @@ namespace Clase
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- AD/20220912/Clase/Clase/Form1.cs
+++ AD/20220912/Clase/Clase/Form1.cs
@@ -10,8 +10,10 @@ using System.Windows.Forms;
 
 namespace Clase
 {
+    
     public partial class Formulario : Form
     {
+       
         public Formulario()
         {
             InitializeComponent();
@@ -19,14 +21,10 @@ namespace Clase
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-          
-        }
+        {}
 
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        {}
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -59,6 +57,43 @@ namespace Clase
         private void button3_Click(object sender, EventArgs e)
         {
             Ejercicio3 formulario = new Ejercicio3();
+            formulario.ShowDialog();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Ejercicio4 formulario = new Ejercicio4();
+            formulario.ShowDialog();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Label lbl = new Label();
+            lbl.Text = "hola";
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Ejercico5 formulario = new Ejercico5();
+            formulario.ShowDialog();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Ejercicio7 formulario = new Ejercicio7();
             formulario.ShowDialog();
 
         }
