e1727b172b0a301b9b5ec5cb73a7cdc52111e6c1
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2022-10-12 12:21:03 +0200

    Practica Acceso a Datos (Se imprime todo el Xml)

---
+++ AD/20220912/Aventura/src/main/java/org/example/OpcionJABX.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+public class OpcionJABX {
+
+    //Atributos
+    private int id;
+    private String textoOpcion;
+    private int resultado;
+
+     //Constructores
+    public OpcionJABX(){}
+    public OpcionJABX(int id, String textoOpcion, int resultado){
+        this.id = id;
+        this.textoOpcion = textoOpcion;
+        this.resultado = resultado;
+    }
+
+    //Getters
+
+    public String getTextoOpcion(){
+        return textoOpcion;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getResultado(){
+        return resultado;
+    }
+
+    @XmlAttribute(name = "id")
+    public void setId(int id) {
+        this.id = id;
+    }
+    @XmlAttribute(name = "resultado")
+    public void setResultado(int resultado) {
+        this.resultado = resultado;
+    }
+    @XmlAttribute(name = "texto")
+    public void setTextoOpcion(String textoOpcion) {
+        this.textoOpcion = textoOpcion;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Id: " + this.id + ", Texto: " + this.textoOpcion + ", Resultado: " + this.resultado + " || ";
+    }
+}
