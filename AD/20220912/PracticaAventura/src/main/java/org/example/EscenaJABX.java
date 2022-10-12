1bad4c3a4380fef7f588a5ed93a2a9e8a0d4c12e
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2022-10-12 12:45:53 +0200

    Esto no me sirve para nada
    j

---
+++ AD/20220912/PracticaAventura/src/main/java/org/example/EscenaJABX.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
+
+public class EscenaJABX {
+
+    private int codigoEscena;
+    private String texto;
+    private ArrayList<OpcionJABX> listadoOpciones;
+
+
+
+
+    //Constructores
+    public EscenaJABX(){}
+
+    public EscenaJABX( String texto, int codigoEscena, ArrayList<OpcionJABX> listadoOpciones){
+        this.texto = texto;
+        this.codigoEscena = codigoEscena;
+        this.listadoOpciones = listadoOpciones;
+    }
+
+    //Getters
+    public ArrayList<OpcionJABX> getListadoOpciones(){
+        return listadoOpciones;
+    }
+
+    public int getCodigoEscena() {
+        return codigoEscena;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    //Setters
+    @XmlAttribute(name = "codigo")
+    public void setCodigoEscena(int codigoEscena) {
+        this.codigoEscena = codigoEscena;
+    }
+
+    @XmlElement
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+
+    @XmlElement(name = "opcion")
+    public void setListadoOpciones(ArrayList<OpcionJABX> listadoOpciones) {
+        this.listadoOpciones = listadoOpciones;
+    }
+
+
+    //ToString
+
+    @Override
+    public String toString() {
+        String resultado =  "\n Codigo escena: " +  this.codigoEscena + "\n " + this.texto + "\n " + this.listadoOpciones ;
+        return resultado;
+    }
+}
---
+++ AD/20220912/PracticaAventura/src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static  void main(String[] args ) {
+
+        try{
+            mostrarEscenas();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void mostrarEscenas(){
+        //Ruta del path
+        String ruta = "C:\\Users\\usuario\\Documents\\GitHub\\Daniel-Mallo-Lopez\\AD\\20220912\\PracticaAventura\\src\\main\\java\\org\\example\\Aventura.xml";
+        // Creamos el arrayList en donde se guardaran las escenas
+
+        try {
+            File file = new File(ruta);
+            if(file.exists()){
+                JAXBContext context = JAXBContext.newInstance(Aventura.class);
+                Unmarshaller unmarshaller = context.createUnmarshaller();
+                Aventura aventura = (Aventura) unmarshaller.unmarshal(file);
+                System.out.println(aventura);
+
+            }else{
+                System.out.println("Fichero no encontrado");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("SE HA PRODUCIDO UN ERROR EN LA OPERACION");
+
+        }
+    }
+}
