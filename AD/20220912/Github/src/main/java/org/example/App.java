7d8344c586a3d6d4386ac0034c3bdf6b70344f9c
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2022-10-22 20:14:00 +0200

    1

---
+++ AD/20220912/Github/src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Esto es una prueba");
+    }
+}
